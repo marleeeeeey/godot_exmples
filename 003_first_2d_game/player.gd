b7a64d02005458c13d6c24374e7e21c2c21a9400
marleeeeeey <marleeeeeey@gmail.com>
2024-05-07 00:02:35 +0400

    Player movebale via WASD, arrows and controller.

--- 003_first_2d_game/player.gd
+++ 003_first_2d_game/player.gd
@@ -5,9 +5,30 @@ var screen_size # Size of the game window.
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	screen_size = get_viewport_rect().size
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+
+	# Calculate the player's movement vector.
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	# Manage the player's animation (and normalize the velocity vector).
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
+	# Move the player.
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
