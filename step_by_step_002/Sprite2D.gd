154345fc69bbbcad622fdcebfaf05346c4398c2f
marleeeeeey <marleeeeeey@gmail.com>
2024-05-06 15:06:01 +0400

    Move the sprite on UP key

--- step_by_step_002/Sprite2D.gd
+++ step_by_step_002/Sprite2D.gd
@@ -7,6 +7,8 @@ func _init() -> void:
 	print("Hello, world")
 
 func _process(delta: float) -> void:
+
+	# Calculate the rotation
 	var direction = 0
 	if Input.is_action_pressed("ui_left"):
 		direction = -1
@@ -14,5 +16,10 @@ func _process(delta: float) -> void:
 		direction = 1
 	rotation += direction * angular_speed * delta
 
-	var velocity = Vector2.UP.rotated(rotation) * speed
+	# Calculate the velocity
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity += Vector2.UP.rotated(rotation) * speed
+
+	# Move the sprite
 	position += velocity * delta
