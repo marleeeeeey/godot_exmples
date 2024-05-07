dfa155f7a5a04a44ba0093da9e76d64f2aa70520
marleeeeeey <marleeeeeey@gmail.com>
2024-05-07 12:03:44 +0400

    Refactoring.

--- 003_first_2d_game/src/main.gd
+++ 003_first_2d_game/src/main.gd
@@ -3,14 +3,6 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-## Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-#new_game()
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#pass
-
 
 func game_over() -> void:
 	$ScoreTimer.stop()
--- 003_first_2d_game/src/player.gd
+++ 003_first_2d_game/src/player.gd
@@ -56,7 +56,8 @@ func _on_body_entered(body: Node2D) -> void:
 	hide()  # Player disappears after being hit.
 	hit.emit()
 
-	# Disabling the area's collision shape can cause an error if it happens in the middle of the engine's collision processing.
+	# Disabling the area's collision shape can cause an error
+	# if it happens in the middle of the engine's collision processing.
 	# Using set_deferred() tells Godot to wait to disable the shape until it's safe to do so.
 	$CollisionShape2D.set_deferred("disabled", true)
 
