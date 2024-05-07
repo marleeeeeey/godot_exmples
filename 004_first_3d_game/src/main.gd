13f246159b0f13ace4edb192b356bfb0a9f0d078
marleeeeeey <marleeeeeey@gmail.com>
2024-05-08 02:48:11 +0400

    Add HUD.

--- 004_first_3d_game/src/main.gd
+++ 004_first_3d_game/src/main.gd
@@ -3,6 +3,16 @@ extends Node
 @export var mob_scene: PackedScene
 
 
+func _ready():
+	$UserInterface/Retry.hide()
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		# This restarts the current scene.
+		get_tree().reload_current_scene()
+
+
 func _on_mob_timer_timeout() -> void:
 	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
@@ -20,6 +30,10 @@ func _on_mob_timer_timeout() -> void:
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
 
+	# We connect the mob to the score label to update the score upon squashing one.
+	mob.squashed.connect($UserInterface/ScoreLabel._on_mob_squashed.bind())
+
 
 func _on_player_hit() -> void:
 	$MobTimer.stop()
+	$UserInterface/Retry.show()
