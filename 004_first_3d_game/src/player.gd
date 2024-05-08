68cc91373fb2d89ce496dcdad102f9634697fd42
marleeeeeey <marleeeeeey@gmail.com>
2024-05-08 17:15:25 +0400

    Different speed of animation when moving. Add jump arc.

--- 004_first_3d_game/src/player.gd
+++ 004_first_3d_game/src/player.gd
@@ -41,6 +41,9 @@ func _physics_process(delta):
 		direction = direction.normalized()
 		# Setting the basis property will affect the rotation of the node.
 		$Pivot.basis = Basis.looking_at(direction)
+		$AnimationPlayer.speed_scale = 4
+	else:
+		$AnimationPlayer.speed_scale = 1
 
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
@@ -80,6 +83,9 @@ func _physics_process(delta):
 				break
 
 	move_and_slide()
+
+	# Make the character arc when jumping.
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
 
 
 # And this function at the bottom.
