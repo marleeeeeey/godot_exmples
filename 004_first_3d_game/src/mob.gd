7109940f6f84ef139c873409ddd8cf1a6c051b02
marleeeeeey <marleeeeeey@gmail.com>
2024-05-08 20:23:57 +0400

    Refactoring.

--- 004_first_3d_game/src/mob.gd
+++ 004_first_3d_game/src/mob.gd
@@ -5,6 +5,7 @@ signal squashed
 
 # Minimum speed of the mob in meters per second.
 @export var min_speed = 10
+
 # Maximum speed of the mob in meters per second.
 @export var max_speed = 18
 
--- 004_first_3d_game/src/player.gd
+++ 004_first_3d_game/src/player.gd
@@ -10,15 +10,15 @@ signal hit
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 
-# The target_velocity is a 3D vector combining a speed with a direction.
-var target_velocity = Vector3.ZERO
-
 # Vertical impulse applied to the character upon jumping in meters per second.
 @export var jump_impulse = 20
 
 # Vertical impulse applied to the character upon bouncing over a mob in
 # meters per second.
 @export var bounce_impulse = 16
+
+# The target_velocity is a 3D vector combining a speed with a direction.
+var target_velocity = Vector3.ZERO
 
 
 func _physics_process(delta):
@@ -53,9 +53,6 @@ func _physics_process(delta):
 	if not is_on_floor():  # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 
-	# Moving the Character
-	velocity = target_velocity
-
 	# Jumping.
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		target_velocity.y = jump_impulse
@@ -82,6 +79,8 @@ func _physics_process(delta):
 				# Prevent further duplicate calls.
 				break
 
+	# Moving the Character
+	velocity = target_velocity
 	move_and_slide()
 
 	# Make the character arc when jumping.
