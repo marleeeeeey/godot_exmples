ad18db0537eeda1723eadec940fa7bb32e1afe9b
marleeeeeey <marleeeeeey@gmail.com>
2024-05-12 12:48:35 +0400

    Implement movement to mouse click pos.

--- 007_navigation/assets/scene_spaceship/spaceship.gd
+++ 007_navigation/assets/scene_spaceship/spaceship.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 var target_position = Vector2.ZERO
+
 
 func _input(event):
 	# Mouse in viewport coordinates.
@@ -12,5 +12,12 @@ func _input(event):
 		print("Mouse Click/Unclick at: ", event.position)
 		target_position = event.position
 
+
 func _physics_process(delta: float) -> void:
+	var diff = target_position - global_position
+	if diff.length() < 5:
+		return
+
+	var direction = (target_position - global_position).normalized()
+	velocity = direction * SPEED
 	move_and_slide()
