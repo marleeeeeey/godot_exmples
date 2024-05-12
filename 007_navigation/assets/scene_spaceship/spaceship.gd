2e68b08e60e2d68188f0665c01ea876597756baf
marleeeeeey <marleeeeeey@gmail.com>
2024-05-12 20:52:18 +0400

    Implement navigation map and navigation.

--- 007_navigation/assets/scene_spaceship/spaceship.gd
+++ 007_navigation/assets/scene_spaceship/spaceship.gd
@@ -1,23 +1,33 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-var target_position = Vector2.ZERO
+@export var speed = 300
 
 
 func _input(event):
 	# Mouse in viewport coordinates.
 	if event is InputEventMouseButton:
-		print("Mouse Click/Unclick at: ", event.position)
-		target_position = event.position
+		# World2D - holds all components of a 2D world, such as a canvas and a physics space.
+		var map = get_world_2d().navigation_map
+
+		# Set the point to navigate to.
+		var closest_point_on_nav_map = NavigationServer2D.map_get_closest_point(map, event.position)
+		$NavigationAgent2D.target_position = closest_point_on_nav_map
 
 
 func _physics_process(delta: float) -> void:
-	var diff = target_position - global_position
-	if diff.length() < 5:
+	if $NavigationAgent2D.is_navigation_finished():
 		return
 
-	var direction = (target_position - global_position).normalized()
-	velocity = direction * SPEED
+	# Calculate the next path position.
+	var next_path_position = $NavigationAgent2D.get_next_path_position()
+
+	# Rotate towards the next path position. X axis is the forward direction.
+	look_at(next_path_position)
+
+	# Calc velocity towards the next path position.
+	var diff = next_path_position - global_position
+	var direction = diff.normalized()
+	velocity = direction * speed
+
+	# Move the character.
 	move_and_slide()
