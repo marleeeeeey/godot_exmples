664878d92382276715a68d7c824e3f4c62695b86
marleeeeeey <marleeeeeey@gmail.com>
2024-05-15 02:32:51 +0400

    Change zoom via mouse. Update aim.png.

--- 008_tilemap_navigation/common/scene_debug_aim/debug_aim.gd
+++ 008_tilemap_navigation/common/scene_debug_aim/debug_aim.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 
 @export var debug_text = ""
+@export var show_text = false
 
 
 func _ready() -> void:
@@ -8,6 +9,13 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
+	if show_text:
+		$DebugLabel.show()
+		$PosLabel.show()
+	else:
+		$DebugLabel.hide()
+		$PosLabel.hide()
+
 	$PosLabel.text = "l: %s g: %s" % [position, global_position]
 	$DebugLabel.text = debug_text
 
